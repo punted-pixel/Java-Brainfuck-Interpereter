6e230854953d4e008ee4e299b087ffd13b86c586
Andrew Spano <aspano@localhost.localdomain>
2016-03-18 19:02:53 -0400

    renamed misspelled code execution manager

--- src/CodeExceutionManager.java
+++
@@ -1,8 +0,0 @@
-/*
- * Created by Andrew Spano, 2016
- * Handles the execution of the brain fuck program
- * including the location of the code pointer, and which instruction to execute next
- */
-public class CodeExceutionManager {
-
-}
---
+++ src/CodeExecutionManager.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+/*
+ * Created by Andrew Spano, 2016
+ * Handles the execution of the brain fuck program
+ * including the location of the code pointer, and which instruction to execute next
+ */
+public class CodeExecutionManager {
+	private String sourceText;
+	private int codePointer; //keeps track of where in the code the program is
+	private Stack<Integer> CodePointerStack;
+	
+	public CodeExecutionManager(String sourceText) {
+		
+	}
+}
