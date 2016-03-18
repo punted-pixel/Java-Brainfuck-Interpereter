88b8cb9043d5d35a1bbabd32841f2cc38752f086
Andrew Spano <aspano@localhost.localdomain>
2016-03-18 18:25:22 -0400

    Added constructor for text parser

--- src/TextParser.java
+++ src/TextParser.java
@@ -1,10 +1,24 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class TextParser {
 
-	private File file;
+	//private File file;
 	
 	private Scanner scanner;
+	
+	
+	public TextParser(File file) {
+		try {
+			FileReader inputReader = new FileReader(file);
+			scanner = new Scanner(inputReader);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 	
 }
