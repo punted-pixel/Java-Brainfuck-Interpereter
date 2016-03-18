1779125ff5807092d8b224408dddb6ead08dd8dd
Andrew Spano <aspano@localhost.localdomain>
2016-03-18 18:51:28 -0400

    added loop that goes through lines in scanner and builds a string from it

--- src/TextParser.java
+++ src/TextParser.java
@@ -1,3 +1,8 @@
+/*
+ * Created by Andrew Spano, 2016
+ * Parses the text in the brain fuck source file and translates it into brain fuck instruction
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,17 +13,26 @@ public class TextParser {
 	//private File file;
 	
 	private Scanner scanner;
+	private String sourceText; //string containing the text in the brain fuck program
 	
 	
 	public TextParser(File file) {
+		this.sourceText = "";
 		try {
 			FileReader inputReader = new FileReader(file);
-			scanner = new Scanner(inputReader);
+			this.scanner = new Scanner(inputReader);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.exit(1);
 		}
+		
+		while(scanner.hasNextLine()) {
+			this.sourceText += scanner.nextLine();
+		}
+	}
+	
+	public String getSourceText() {
+		return sourceText;
 	}
 	
 }
