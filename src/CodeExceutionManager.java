226c30fa34c7fa02d76fce91a8c2b1f648eee590
Andrew Spano <aspano@localhost.localdomain>
2016-03-18 18:53:19 -0400

    Added a class to handle the execution of the brain fuck code

---
+++ src/CodeExceutionManager.java
@@ -0,0 +1,8 @@
+/*
+ * Created by Andrew Spano, 2016
+ * Handles the execution of the brain fuck program
+ * including the location of the code pointer, and which instruction to execute next
+ */
+public class CodeExceutionManager {
+
+}
