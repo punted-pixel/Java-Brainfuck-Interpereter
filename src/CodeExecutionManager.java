78c5b3ce4968f2be4af01f1e3f6f0a6ed7775a43
Andrew Spano <aspano@localhost.localdomain>
2016-03-19 00:25:57 -0400

    added datamanager field and execute instruction method to codeexecution manager

--- src/CodeExecutionManager.java
+++ src/CodeExecutionManager.java
@@ -9,10 +9,47 @@ public class CodeExecutionManager {
 	private String sourceText;
 	private int codePointer; //keeps track of where in the code the program is
 	private Stack<Integer> codePointerStack;
+	private DataStateManager dataStateManager;
 	
 	public CodeExecutionManager(String sourceText) {
 		this.sourceText = sourceText;
 		this.codePointer = 0;
 		this.codePointerStack = new Stack<Integer>();
+		this.dataStateManager = new DataStateManager();
 	}
+	
+	public void  stepInstruction() {
+		/*
+		 * set code pointer to the next instruction
+		 */
+		codePointer++;
+	}
+	
+	public void executeInstruction() {
+		char instruction = sourceText.charAt(codePointer);
+		switch (instruction) {
+		case '+':
+			dataStateManager.increment();
+			break;
+		case '-':
+			dataStateManager.decrement();
+			break;
+		case '>':
+			dataStateManager.shiftPointerRight();
+			break;
+		case '<':
+			dataStateManager.shiftPointerLeft();
+			break;
+		case '.':
+			dataStateManager.printCellValue();
+			break;
+		case ',':
+			dataStateManager.inputCellValue();
+			break;
+		
+			
+		}
+	}
+	
 }
+
